fd4a575b2bf6ef68e440b565490121bae8f921b6
zhaojiang chang <zhaojiangchangnz@gmail.com>
2016-04-17 02:31:16 +1200

    crate taxonomy graph database
    the reason to create graph database is to easy to find the relationship
    between parent and child taxonomy node, when create relationship
    between web services

--- neo4j/src/main/java/org/neo4j/neo4j/Main.java
+++ neo4j/src/main/java/org/neo4j/neo4j/Main.java
@@ -26,6 +26,7 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 /**
@@ -40,27 +41,30 @@ public class Main
 	public static final int RELIABILITY = 3;
 	public static Map<String, ServiceNode> serviceMap = new HashMap<String, ServiceNode>();
 
-	
-	private static final String Neo4j_DBPath = "/Users/JackyChang/Engr489-WebServicesComposition-Neo4j/neo4j/database";
+	public Map<String, TaxonomyNode> taxonomyMap = new HashMap<String, TaxonomyNode>();
+
+	private static final String Neo4j_ServicesDBPath = "/Users/JackyChang/Engr489-WebServicesComposition-Neo4j/neo4j/database/services";
+	private static final String Neo4j_TaxonomyDBPath = "/Users/JackyChang/Engr489-WebServicesComposition-Neo4j/neo4j/database/taxonomy";
 	List<Node> neo4jServiceNodes;
+	List<Node> neo4jTaxonomyNodes;
 	Relationship relation;
 	GraphDatabaseService graphDatabaseService;
 	private static enum RelTypes implements RelationshipType{
-		KNOWS
+		PARENT, CHILD
 	}
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
     	Main neo4jwsc = new Main();
     	neo4jwsc.parseWSCServiceFile("test_serv.xml");
-    	neo4jwsc.createDatabase(serviceMap);
+    	neo4jwsc.createServicesDatabase(serviceMap);
+    	neo4jwsc.parseWSCTaxonomyFile("test_taxonomy.xml");
 //    	hello.removeData();
+    	neo4jwsc.setServicesRelationship(neo4jwsc.neo4jServiceNodes);
     	neo4jwsc.shutdown();
     	
     }
-    @SuppressWarnings("deprecation")
-	void createDatabase(Map<String, ServiceNode> serviceMap){
-    	graphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase(Neo4j_DBPath);
+	void createServicesDatabase(Map<String, ServiceNode> serviceMap){
+    	graphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase(Neo4j_ServicesDBPath);
     	Transaction transaction = graphDatabaseService.beginTx();
     	neo4jServiceNodes = new ArrayList<Node>();
     	try{
@@ -69,18 +73,15 @@ public class Main
     		    ServiceNode value = entry.getValue();
     		    // do what you have to do here
     		    // In your case, an other loop.
-    		    Node n = graphDatabaseService.createNode();
+    		    Node service = graphDatabaseService.createNode();
     		    Label nodeLable = DynamicLabel.label(key);
-    		    n.addLabel(nodeLable);
-    		    n.setProperty("name", key);
-    		    n.setProperty("qos", value.getQos());
-    		    n.setProperty("inputs", value.getInputs());
-    		    n.setProperty("outputs", value.getOutputs());
-    		    neo4jServiceNodes.add(n);
+    		    service.addLabel(nodeLable);
+    		    service.setProperty("name", key);
+    		    service.setProperty("qos", value.getQos());
+    		    service.setProperty("inputs", value.getInputs());
+    		    service.setProperty("outputs", value.getOutputs());
+    		    neo4jServiceNodes.add(service);
     		}
-    		System.out.println(neo4jServiceNodes.size());
-
-   		
 //    		relation = first.createRelationshipTo(second, RelTypes.KNOWS);
 //    		relation.setProperty("relationship", "knows");
 //    		System.out.println(first.getProperty("name").toString() +"  "+relation.getProperty("relationship").toString()+"  "+second.getProperty("name").toString());
@@ -107,6 +108,9 @@ public class Main
     	graphDatabaseService.shutdown();
     	System.out.println("Neo4j database is shutdown");
     }
+    void setServicesRelationship(List<Node> neo4jServiceNodes){
+    	
+    }
     /**
 	 * Parses the WSC Web service file with the given name, creating Web
 	 * services based on this information and saving them to the service map.
@@ -131,8 +135,6 @@ public class Main
 				Element eElement = (Element) nNode;
 
 				String name = eElement.getAttribute("name");
-				System.out.println(name);
-
 //				if (!runningOwls) {
 					qos[TIME] = Double.valueOf(eElement.getAttribute("Res"));
 					qos[COST] = Double.valueOf(eElement.getAttribute("Pri"));
@@ -176,6 +178,91 @@ public class Main
 		}
 		catch (SAXException e) {
 			System.out.println("Service file parsing failed...");
+		}
+	}
+		
+	/**
+	 * Parses the WSC taxonomy file with the given name, building a
+	 * tree-like structure.
+	 *
+	 * @param fileName
+	 */
+	@SuppressWarnings("deprecation")
+	private void parseWSCTaxonomyFile(String fileName) {
+		try {
+			File fXmlFile = new File(fileName);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			NodeList taxonomyRoots = doc.getChildNodes();
+			graphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase(Neo4j_TaxonomyDBPath);
+			Transaction transaction = graphDatabaseService.beginTx();
+			neo4jTaxonomyNodes = new ArrayList<Node>();
+			try{
+				processTaxonomyChildren(null, taxonomyRoots, null);
+				transaction.success();
+			}finally{
+				transaction.finish();
+			}
+			
+		}
+
+		catch (ParserConfigurationException e) {
+			System.err.println("Taxonomy file parsing failed...");
+		}
+		catch (SAXException e) {
+			System.err.println("Taxonomy file parsing failed...");
+		}
+		catch (IOException e) {
+			System.err.println("Taxonomy file parsing failed...");
+		}
+	}
+
+	/**
+	 * Recursive function for recreating taxonomy structure from file.
+	 *
+	 * @param parent - Nodes' parent
+	 * @param nodes
+	 */
+	private void processTaxonomyChildren(TaxonomyNode parent, NodeList nodes, Node parentGNode) {
+		Node taxonomyGNode = null;
+		if (nodes != null && nodes.getLength() != 0) {
+			for (int i = 0; i < nodes.getLength(); i++) {
+				
+				org.w3c.dom.Node ch = nodes.item(i);
+
+				if (!(ch instanceof Text)) {
+					Element currNode = (Element) nodes.item(i);
+					String value = currNode.getAttribute("name");
+					TaxonomyNode taxNode = taxonomyMap.get( value );
+					if (taxNode == null) {
+						System.out.println(taxNode==null);
+						System.out.println(value);
+						taxNode = new TaxonomyNode(value);
+						taxonomyMap.put( value, taxNode );
+						
+						taxonomyGNode = graphDatabaseService.createNode();
+						if(value!=""){
+						    Label nodeLable = DynamicLabel.label(value);
+						    taxonomyGNode.addLabel(nodeLable);
+						}
+					    taxonomyGNode.setProperty("name", value);
+					    neo4jTaxonomyNodes.add(taxonomyGNode);
+					}
+					if (parent != null) {
+						taxNode.parents.add(parent);
+						parent.children.add(taxNode);
+						relation = taxonomyGNode.createRelationshipTo(parentGNode, RelTypes.CHILD);
+						relation = parentGNode.createRelationshipTo(taxonomyGNode, RelTypes.PARENT);
+//						relation.setProperty("relationship", "knows");
+//						System.out.println(first.getProperty("name").toString() +"  "+relation.getProperty("relationship").toString()+"  "+second.getProperty("name").toString());
+
+						
+					}
+					NodeList children = currNode.getChildNodes();
+					processTaxonomyChildren(taxNode, children, taxonomyGNode);
+				}
+			}
 		}
 	}
 	public String[] increaseArray(String[] theArray)
